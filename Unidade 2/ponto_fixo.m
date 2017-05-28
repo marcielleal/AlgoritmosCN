e07779cfc4bfe91e997e11a53c32b4cb8e113632
Igor Barbosa Nogueira <igor@igorPC.home>
2017-05-28 04:16:07 -0300

    Adicionado ponto_fixo.m

---
+++ Unidade 2/ponto_fixo.m
@@ -0,0 +1,13 @@
+﻿function[raiz] = ponto_fixo(f, g, erro,a,b)
+    _p = (a + b)/2;
+    p = g(_p);
+    while  abs(f(_p)) > erro
+        _p = p;
+        p = g(_p);
+    end
+    raiz=p;
+end
+
+disp(ponto_fixo(@(x)(x^3+4*x^2-10), @(y)(y-((y^3+4*y^2-10)/(3*y^2+8*y))), 0.1, 1.0, 2.0 ));
+
+% g(x) pode ser isso (10/(4+y))^0.5 
