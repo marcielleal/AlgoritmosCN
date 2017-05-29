fccd790167da683c1bdc10dced4109c1baf63e08
Igor Barbosa Nogueira <minigor@imd.local>
2017-05-29 19:43:48 -0300

    Adicionado Bisseção-Newton

---
+++ Unidade 2/bissecao_newton.m
@@ -0,0 +1,28 @@
+﻿function[raiz] = corda(f,erro,a,b)
+   
+    _x = (a+b)/2;
+        
+    delta = -(f(_x)/deriv(f, _x));
+   
+    while abs(delta/_x) > erro
+    
+        x = _x + delta;    
+    
+        if x > a && x < b
+    		if f(a)*f(x) > 0
+    		    a = x;
+    		else
+    		    b = x;
+    		end
+        end
+        
+        _x = x;
+        
+        delta = -(f(_x)/deriv(f, _x));
+    end
+    raiz = _x;
+end
+
+tic()
+disp(corda(@(x)(x^2-2), 0.00001, -89.0, 100.0 ));
+toc()
