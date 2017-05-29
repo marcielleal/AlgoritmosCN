f68154c21e54adce5d424586e26c8a151bfd05d9
Igor Barbosa Nogueira <minigor@imd.local>
2017-05-29 20:46:15 -0300

    Adicionando troca_sinal2.m

---
+++ Unidade 2/troca_sinal2.m
@@ -0,0 +1,11 @@
+﻿function[intervalo] = troca_sinal(f,x,p)
+    
+    while (f(x)*f(x+p) > 0)
+        x = x + p;
+    end
+    
+   intervalo = [x x+p];
+end
+
+clc()
+disp(troca_sinal(@(x)(x^2-2), 0, 1));
